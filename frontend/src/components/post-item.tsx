3dc88a49168f18239b7e6b646e6b3551d516a5f7
Alexey Efimov <alex-vngd@yandex.ru>
2025-02-03 01:23:06 +0300

    fixes

--- frontend/src/components/post-item.tsx
+++ frontend/src/components/post-item.tsx
@@ -20,9 +20,9 @@ const PostItem: FC<PostItemProps> = ({date, data, author, owner, onClose, handle
       <span>{date}</span>
       <span>{data}</span>
       {media ? ( <span>{media.endsWith('.mp4') ? (
-            <video className={styles.media} src={`http://${baseUrl}/${media.slice(10)}`} controls width="100%" />
+            <video className={styles.media} src={`${baseUrl}/${media.slice(10)}`} controls width="100%" />
           ) : (
-            <img className={styles.media} src={`http://${baseUrl}/${media.slice(10)}`} alt="Post media" width="100%" />
+            <img className={styles.media} src={`${baseUrl}/${media.slice(10)}`} alt="Post media" width="100%" />
           )}</span>) : (<span>Здесь может быть медиафайл</span>)}
       <span>{author}</span>
       <button disabled={user?.id !== owner} onClick={onClose} className={styles.trashButton}>&#10006;</button>
