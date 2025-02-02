38dd21cc3f14d6be7b28d57ed804690c912e04bb
Alexey Efimov <alex-vngd@yandex.ru>
2025-02-02 22:47:00 +0300

    fixes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,7 +16,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(routes);
-app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+const dirName = path.join(__dirname, 'uploads');
+console.log(dirName);
 
 myDataSource
   .initialize()
